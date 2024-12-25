b0c503cc50cb54f0f6edaf5361d87e9664e395cf
x-macas <vksharma11235@gmail.com>
2024-12-26 00:40:02 +0530

    Footer description changed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
             className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
           >
           </Link>
-          <span className="text-muted">© 2024 Go Food, Inc</span>
+          <span className="text-muted">© {new Date().getFullYear()} Webomato. All Rights Reserved.</span>
         </div>
 
       </footer>
